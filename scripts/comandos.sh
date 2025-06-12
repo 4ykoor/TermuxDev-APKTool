01499e3eb2d960308376478276c13ac0cdcc4ca8
4ykoor <4ykoor@gmail.com>
2025-06-12 14:11:56 -0300

    Update comandos.sh

--- scripts/comandos.sh
+++ scripts/comandos.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+verde="\033[1;32m"
+neutro="\033[0m"
+
+clear
+echo -e "${verde}Opções úteis para programadores${neutro}"
+echo "----------------------------------------"
+echo "Essa seção está em desenvolvimento..."
+echo
+read -p "Pressione Enter para voltar ao menu..." nada
