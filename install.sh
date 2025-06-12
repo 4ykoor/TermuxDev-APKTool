734495761f5210a83ad45a6dfc4adf30220b20c4
4ykoor <4ykoor@gmail.com>
2025-06-12 07:32:22 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+pkg update -y && pkg upgrade -y
+
+curl -sLo ~/inter-menu.sh https://raw.githubusercontent.com/4ykoor/devpreguicoso/main/inter-menu.sh
+chmod +x ~/inter-menu.sh
+
+bash ~/inter-menu.sh
 
